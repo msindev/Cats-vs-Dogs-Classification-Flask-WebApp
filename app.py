fd95111f6f2d80113cd2152a2f44b7799e309489
Mohit Singh <mohitwildbeast@gmail.com>
2019-06-02 16:21:21 +0530

    Add app files

---
+++ app.py
@@ -0,0 +1,86 @@
+import os
+from flask import Flask, render_template, request
+from flask import send_from_directory
+from keras.models import load_model
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+from keras import backend as K
+
+import numpy as np
+import tensorflow as tf
+
+app = Flask(__name__)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+# UPLOAD_FOLDER = dir_path + '/uploads'
+# STATIC_FOLDER = dir_path + '/static'
+UPLOAD_FOLDER = 'uploads'
+MODEL_FOLDER = 'static/model'
+
+# load and prepare the image
+def load_image(filename):
+    # load the image
+    img = load_img(filename, target_size=(224, 224))
+    # convert to array
+    img = img_to_array(img)
+    # reshape into a single sample with 3 channels
+    img = img.reshape(1, 224, 224, 3)
+    # center pixel data
+    img = img.astype('float32')
+    img = img - [123.68, 116.779, 103.939]
+    return img
+
+# load an image and predict the class
+def predict(filename):
+    # load the image
+    img = load_img(filename, target_size=(224, 224))
+    # convert to array
+    img = img_to_array(img)
+    # reshape into a single sample with 3 channels
+    img = img.reshape(1, 224, 224, 3)
+    # center pixel data
+    img = img.astype('float32')
+    img = img - [123.68, 116.779, 103.939]
+    # load model
+    model = load_model(MODEL_FOLDER + '/cats_vs_dogs_model.h5')
+    # predict the class
+    result = model.predict(img)
+    result = result.flatten()
+    result = round(result[0])
+    K.clear_session()
+    return result
+
+
+# home page
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+
+# procesing uploaded file and predict it
+@app.route('/upload', methods=['POST','GET'])
+def upload_file():
+
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        file = request.files['image']
+        full_name = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(full_name)
+
+        indices = {0: 'Cat', 1: 'Dog'}
+        result = predict(full_name)
+
+        #accuracy = round(result[0][predicted_class] * 100, 2)
+        label = indices[result]
+
+    return render_template('predict.html', image_file_name = file.filename, label = label)
+
+
+@app.route('/uploads/<filename>')
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
