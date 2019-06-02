7e0aee3400c1e7d3823dbf239b1115ffd299f687
Mohit Singh <mohitwildbeast@gmail.com>
2019-06-02 09:24:27 +0530

    Predict the image labels

---
+++ temp/predict.py
@@ -0,0 +1,31 @@
+# make a prediction for a new image.
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+
+# load and prepare the image
+def load_image(filename):
+    # load the image
+    img = load_img(filename, target_size=(224, 224))
+    # convert to array
+    img = img_to_array(img)
+    # reshape into a single sample with 3 channels
+    img = img.reshape(1, 224, 224, 3)
+    # center pixel data
+    img = img.astype('float32')
+    img = img - [123.68, 116.779, 103.939]
+    return img
+
+# load an image and predict the class
+def run_example():
+    # load the image
+    img = load_image('test/9.jpg')
+    # load model
+    model = load_model('final_model.h5')
+    # predict the class
+    result = model.predict(img)
+    result = result.flatten()
+    print(round(result[0]))
+
+# entry point, run the example
+run_example()
